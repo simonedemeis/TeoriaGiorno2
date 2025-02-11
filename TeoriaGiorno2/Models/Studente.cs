2f706d0c82f6bbab2353c297c0ec4c32253ebeac
s.demeis <s.demeis@capoweb.it>
2025-02-11 09:39:11 +0100

    Campi e proprietà

---
+++ TeoriaGiorno2/Models/Studente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeoriaGiorno2.Models
+{
+    public class Studente
+    {
+        private int _matricola;
+        private string _nome = "simone";
+        private string _cognome;
+        private string _facolta;
+
+        public int Matricola
+        {
+            get
+            {
+                return _matricola;
+            }
+            set
+            {
+                _matricola = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+        }
+    }
+}
--- TeoriaGiorno2/Program.cs
+++ TeoriaGiorno2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using TeoriaGiorno2.Models;
+
+Studente studente1 = new Studente()
+{
+    Matricola = 246796
+};
+
+
+Console.WriteLine(studente1.Nome);
+
+
